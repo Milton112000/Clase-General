e65e47908540ee7e3706b7cc52759f5d6f142fdc
milton <69540512+Milton112000@users.noreply.github.com>
2020-08-29 13:32:19 -0600

    HERENCIA CLASE
    aplicar otras tecnicas a la herencia como evaluar con un if y ingresar datos con el teclado

--- herencia2_1/src/main/java/com/mycompany/herencia2_1/NewMain.java
+++ herencia2_1/src/main/java/com/mycompany/herencia2_1/NewMain.java
@@ -18,43 +18,47 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-//    Scanner sc =new Scanner (System.in);
-//        System.out.println ( " ESCRIBE TU NOMBRE " );
-//        String  Nombre  = sc.nextLine();
-//        System.out.println ( " ESCRIBE TU DIRECCION " );
-//        String  Direccion  = sc.nextLine();
-//         System.out.println( " ESCRIBE TU FECHA DE NACIMIENTO " );
-//        String  Fecha  = sc.nextLine();
-//        System.out.println( " ESCRIBE TU EDAD " );
-//        int  Edad  = sc . nextInt ();
-//        
-//         String Genero=sc.nextLine();
-//
-//
-//
-//        profesor profe =  new profesor ();
-//
-//
-//        profe . setNombre ( Nombre );
-//        profe . setDireccion ( Direccion );
-//        profe . setEdad ( Edad );
-//        profe . setFechaNc ( Fecha );
-//        profe . setGenero ( Genero );
-//        System.out.println(profe . esMayorDeEdad ());
-//        
-//        System.out.println ( profe .mostrarProfesor ());
+    Scanner sc =new Scanner (System.in);
+        System.out.println ( " ESCRIBE TU NOMBRE " );
+        String  Nombre  = sc.nextLine();
+        System.out.println ( " ESCRIBE TU DIRECCION " );
+        String  Direccion  = sc.nextLine();
+         System.out.println( " ESCRIBE TU FECHA DE NACIMIENTO " );
+        String  Fecha  = sc.nextLine();
+        System.out.println( " ESCRIBE TU EDAD " );
+        int  Edad  = sc . nextInt ();
+        
+         String Genero=sc.nextLine();
+
+
+
+        profesor profe =  new profesor ();
+
+
+        profe . setNombre ( Nombre );
+        profe . setDireccion ( Direccion );
+        profe . setEdad ( Edad );
+        profe . setFechaNc ( Fecha );
+        profe . setGenero ( Genero );
+        System.out.println(profe . esMayorDeEdad ());
+        
+        System.out.println ( profe .mostrarProfesor ());
 //          System.out.println ( profe.sinEdad());
 //            System.out.println ( profe.sinTitulo());
             
+    // SEGUNDA PRACTICA   
     
-     Scanner s= new Scanner(System.in);
-        System.out.println("Ingrese el costo del veiculo");
-       int  cos = s.nextInt();
-        
-        ejemploClase cod= new ejemploClase();
-       
-         cod.setCosto(cos);
-        System.out.println(cod.VeriCosto());
+    
+//     Scanner s= new Scanner(System.in);
+//        System.out.println("Ingrese el costo del veiculo");
+//       int  cos = s.nextInt();
+//        
+//        ejemploClase cod= new ejemploClase();
+//       
+//         cod.setCosto(cos);
+//        System.out.println(cod.VeriCosto());
+//    }
+    
+
     }
-    
 }
